79173f74735178f0d17b88ea115f2807e8e55951
alex7alves <alex7alves10@hotmail.com>
2019-03-04 09:08:23 -0300

    Transformando lista em array

--- CNN/Upscaling/Rede.py
+++ CNN/Upscaling/Rede.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 
 Autor : Alex Alves
@@ -14,7 +15,7 @@ import keras
 from sklearn.metrics import confusion_matrix, accuracy_score
 
 from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, Conv2DTranspose, UpSampling2D
 from keras.layers.normalization import BatchNormalization
 from keras.preprocessing.image import ImageDataGenerator
 import numpy as np
@@ -31,11 +32,22 @@ def Carregar_imagens(caminho):
         lista_imagens.append(img)
     return lista_imagens
 
-def Normalizar(x):
+def NormalizarLista(x):
     for i in range(len(x)):
         x[i] = x[i].astype('float32')
         x[i] = x[i]/255
     return x
+def Normalizar(x): 
+    x = x.astype('float32')
+    x = x/255
+    return x
+
+def Converter(x):
+    w,h,c=x[0].shape
+    x = np.array(x, dtype=np.uint8)
+    x = x.reshape(x.shape[0],3,w,h)
+    return x
+
 
 # retorna em (w,h,c) do opencv
 entrada_treinamento = Carregar_imagens("Banco/treinamento")
@@ -45,23 +57,32 @@ desejado_teste = Carregar_imagens("Imagens/teste")
 #print(desejado_treinamento[0].shape)
 #print(len(desejado_treinamento))
 
+entrada_treinamento=Converter(entrada_treinamento)
+desejado_treinamento=Converter(desejado_treinamento)
+
+entrada_teste=Converter(entrada_teste)
+desejado_teste=Converter(desejado_teste)
+
+
+
 entrada_treinamento = Normalizar(entrada_treinamento)
 desejado_treinamento = Normalizar(desejado_treinamento)
 entrada_teste = Normalizar(entrada_teste)
 desejado_teste = Normalizar(desejado_teste)
-#print(entrada_teste[0])
 
 
-
-
+#out = Conv2DTranspose(filters=8,kernel_size=(3,3),strides=2,padding='same')(entrada_treinamento)
+#print(out)
 
 w,h,c=entrada_treinamento[0].shape
 #print(w,h,c)
 upscaling = Sequential()
 # Opencv abre imagem de forma invertida (w,h,c)
-upscaling.add(Conv2D(16, (3,3), input_shape = (w, h, c), activation = 'relu'))
+#upscaling.add(Conv2D(16, (3,3), input_shape = (w, h, c), activation = 'relu'))
+#upscaling.add(Conv2DTranspose(filters=8,kernel_size=(3,3),strides=2,padding='same',activation='relu'))
+upscaling.add(Conv2DTranspose(filters=8,kernel_size=(3,3),strides=2,padding='same',activation='relu'))
+#upscaling.add(UpSampling2D(size =( 2,2)))
 upscaling.add(BatchNormalization())
-
 upscaling.add(Flatten())
 
 upscaling.add(Dense(units = 128, activation = 'relu'))
@@ -70,11 +91,16 @@ upscaling.add(Dense(units = 128, activation = 'relu'))
 #upscaling.add(Dropout(0.2))
 upscaling.add(Dense(units = 1, activation = 'sigmoid'))
 
-upscaling.compile(optimizer = 'adam', loss = 'binary_crossentropy',
+#upscaling.compile(optimizer = 'adam', loss = 'binary_crossentropy',
+  #                    metrics = ['accuracy'])
+
+upscaling.compile(optimizer = 'adam', loss = 'mean_squared_error',
                       metrics = ['accuracy'])
 
 
 
+upscaling.fit(entrada_treinamento,desejado_treinamento,batch_size=128,epochs=5,
+              validation_data=(entrada_teste,desejado_teste))
 
 
 
